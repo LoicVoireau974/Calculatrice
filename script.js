b5642f4bd4dc734ce0412bdf97d73c78f27fb40f
Loic Voireau <loicvoireau3@gmail.com>
2026-01-05 21:25:32 +0100

    demandez nombres à l'utilisateur

---
+++ script.js
@@ -0,0 +1,6 @@
+// Demander à l'utilisateur un premier nombre 
+let n1 = prompt("Veuillez saisir un premier nombre : ");
+// Demander à l'utilisateur un deuxième nombre
+let n2 = prompt("Veuillez saisir un deuxième nombre : ");
+console.log(n1);
+console.log(n2);
