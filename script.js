50a1326ffd0df08c057b9e14013cf9a2181afdef
Loic Voireau <loicvoireau3@gmail.com>
2026-01-05 21:29:12 +0100

    Addition des deux nombres

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Demander à l'utilisateur un premier nombre 
-let n1 = prompt("Veuillez saisir un premier nombre : ");
+let n1 = Number(prompt("Veuillez saisir un premier nombre : "));
 // Demander à l'utilisateur un deuxième nombre
-let n2 = prompt("Veuillez saisir un deuxième nombre : ");
-console.log(n1);
-console.log(n2);
+let n2 = Number(prompt("Veuillez saisir un deuxième nombre : "));
+// Faire l'addition de ces deux nombres
+let sum = n1 + n2;
+console.log(sum);
