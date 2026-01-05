360ccec763089265ea2b15b6a77341c533adc5c3
Loic Voireau <loicvoireau3@gmail.com>
2026-01-05 21:55:06 +0100

    calcul en fonction de l'opérateur choisit

--- script.js
+++ script.js
@@ -2,13 +2,31 @@
 let n1 = Number(prompt("Veuillez saisir un premier nombre : "));
 // Demander à l'utilisateur un deuxième nombre
 let n2 = Number(prompt("Veuillez saisir un deuxième nombre : "));
-// Faire l'addition de ces deux nombres
-let sum = n1 + n2;
-// Faire la soustraction de ces deux nombres 
-let sub = n1 - n2;
-// Faire la multiplication de ces deux nombres
-let pro = n1 * n2;
-// Faire la division de ces deux nombres
-let quo = n1 / n2;
+// Déclarer puis initialisez le résultat à 0
+let res = 0;
 // Demandez à l'utilisateur quelle opérateur choisir pour faire le calcul
 let operatorChoice = prompt('Veuillez choisir entre +, -, x, / : ');
+
+switch(operatorChoice) {
+    // Si l'utilisateur choisit + alors additionner
+    case "+":
+        res = n1 + n2;
+        break;
+    // Si l'utilisateur choisit - alors supprimer
+    case "-":
+        res = n1 - n2;
+        break;
+    // Si l'utilisateur choisit x alors multiplier
+    case "x":
+        res = n1 * n2;
+        break;
+    // Si l'utilisateur choisit / alors diviser
+    case "/":
+        res = n1 / n2;
+        break;
+    default:
+    // Si l'utilisateur choisit un opérateur invalide
+        console.log('Opérateur inconnu');
+}
+
+console.log(res);
